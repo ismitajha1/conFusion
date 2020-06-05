3d37478dc6a19441fd20f5489c80775a3c885b52
Ismita Jha <ismitajha1@gmail.com>
2020-06-05 20:53:58 +0545

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    MatToolbarModule,  
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
