a0067c7d04eadfd298a9347ddf185d41ec3d5d19
Ismita Jha <ismitajha1@gmail.com>
2020-06-08 16:51:53 +0545

    SPA Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',  component: MenuComponent },
   { path: 'about', component: AboutComponent},
   { path: 'contactus', component: ContactComponent},
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
