06a74cfbd60d481eb39644fc89920c1e6f9d7d74
Ismita Jha <ismitajha1@gmail.com>
2020-06-08 23:26:21 +0545

    Week2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 
 @NgModule({
@@ -45,7 +46,7 @@ import { PromotionService } from './services/promotion.service';
     MatButtonModule,
     FlexLayoutModule
   ],
-  providers: [DishService,PromotionService],
+  providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
